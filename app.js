26d6c07153dc70002226bae8147ca53e9da3f367
Anna Schneider <anna.schneiderm@gmail.com>
2021-06-30 16:00:16 -0400

    Sketching out game logic

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ let playerScore = 0
 let puterScore = 0
 let playerChoice = undefined
 
+// Function to check for winner
+
+function checkForWinner() {
+
+  if (playerChoice === puterChoice) {
+    result.innerText = "tie"
+  } else if (playerChoice === "scissors" && puterChoice === "paper"
+    || playerChoice === "paper" && puterChoice === "rock"
+    || playerChoice === "rock" && puterChoice === "sciccors") {
+    playerScoreUpdate()
+    result.innerText = "you win"
+  } else {
+    result.innerText = "you lose"
+    puterScoreUpdate
+  }
+}
+
+// Computer's choice
+function putersChoice() {
+
+}
+
+// Updating scores
+function playerScoreUpdate() {
+  playerScore++
+}
+
+function puterScoreUpdate() {
+  puterScore++
+}
